55228820dfd70925dfc24b99c7749b4f3f13381f
Zack Vera <qphys50@gmail.com>
2024-10-25 16:33:10 -0700

    Fixing an issue where white text overrides everything else.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 import Button from "./Button";
 
 function App() {
   return (
     <div>
       <div>
-        <Button success rounded outline>
+        <Button secondary outline rounded>
+          <GoBell />
           Click me!!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy Now!
         </Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
-import className from 'classnames';
+import { twMerge } from "tailwind-merge";
+import className from "classnames";
 
 function Button({
   children,
@@ -10,13 +11,22 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = className("px-3 py-1.5 border", {
-    "border-blue-500 bg-blue-500 text-white": primary,
-    "border-gray-900 bg-gray-900 text-white": secondary,
-    "border-green-500 bg-green-500 text-white": success,
-    "border-yellow-400 bg-yellow-400 text-white": warning,
-    "border-red-500 bg-red-500 text-white": danger,
-  });
+  const classes = twMerge(
+    className("flex items-center px-3 py-1.5 border", {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    }),
+  );
 
   return <button className={classes}>{children}</button>;
 }
@@ -26,8 +36,8 @@ Button.propTypes = {
     const count =
       Number(!!primary) +
       Number(!!secondary) +
-      Number(!!success) +
       Number(!!warning) +
+      Number(!!success) +
       Number(!!danger);
 
     if (count > 1) {
