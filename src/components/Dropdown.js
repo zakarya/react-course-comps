43fee534b64c292e77c345334901f554017d7bd3
Zack Vera <qphys50@gmail.com>
2024-10-28 16:43:01 -0700

    Added functionality to open and close the dropdown based on a boolean value in state.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,21 @@
-function Dropdown() {
-  return <div>Dropdown...</div>;
+import { useState } from "react";
+
+function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}>{option.label}</div>;
+  });
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
