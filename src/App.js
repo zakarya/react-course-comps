6e8dbec0f69a47d61bd25cf08ba9f5ac689745bf
Zack Vera <qphys50@gmail.com>
2024-10-28 14:04:36 -0700

    Setting up the first steps to create the Dropdown component.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: "l2kj5",
-      label: "Can I use React on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
-    {
-      id: "lk2j35lkj",
-      label: "Can I use Javascript on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
-    {
-      id: "l1kj2i0g",
-      label: "Can I use CSS on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
 
-  return <Accordion items={items} />;
+  return <Dropdown options={options} />;
 }
 
 export default App;
