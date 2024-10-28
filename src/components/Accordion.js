0b3b21613dde85c6c0a298c5bc0d0526329a57f2
Zack Vera <qphys50@gmail.com>
2024-10-27 17:46:12 -0700

    Adding styling and more logic to make the current accordion to be closed if clicked again.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,30 +2,40 @@ import { useState } from "react";
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    if (expandedIndex === nextIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(nextIndex);
+    }
+  };
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
     const icon = (
-      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
     );
+
     return (
       <div key={item.id}>
         <div
-          onClick={() => {
-            setExpandedIndex(index);
-          }}
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
         >
-          {icon}
           {item.label}
+          {icon}
         </div>
-        {isExpanded && <div>{item.content}</div>}
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
