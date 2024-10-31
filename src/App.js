b7b81882c3d8a7e9fd334129970666902736ecf5
Zack Vera <qphys50@gmail.com>
2024-10-31 16:35:10 -0700

    Getting the Dropdown to close when clicked anywhere outside the dropdown. Needed to use useRef.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ function App() {
   ];
 
   return (
-    <Dropdown options={options} value={selection} onChange={handleSelect} />
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
   );
 }
 
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handler, true);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
+
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
@@ -26,7 +44,7 @@ function Dropdown({ options, value, onChange }) {
   });
 
   return (
-    <div className="w-48 relative">
+    <div ref={divEl} className="w-48 relative">
       <Panel
         className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
