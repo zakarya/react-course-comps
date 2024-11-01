d57b2677228575ac5250d5fc3081250da57932f3
Zack Vera <qphys50@gmail.com>
2024-11-01 16:31:38 -0700

    Moving the logic for the Dropdown to the DropdownPage.js file from the App.js file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
