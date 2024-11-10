a353630bec22d28d366e9f4c02c4509ab4cbec20
Zack Vera <qphys50@gmail.com>
2024-11-09 20:21:03 -0800

    Starting to add the code to be able to sort values in the table component.

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -12,6 +13,7 @@ function TablePage() {
     {
       label: "Name",
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
       label: "Color",
@@ -20,7 +22,7 @@ function TablePage() {
     {
       label: "Score",
       render: (fruit) => fruit.score,
-      header: () => <th className="bg-red-500">Score</th>,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
@@ -30,7 +32,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
