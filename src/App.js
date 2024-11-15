d4aa75031fcaf60f660371f437e6250a72911ad4
Zack Vera <qphys50@gmail.com>
2024-11-14 22:36:44 -0800

    Added a CounterPage.js and created a custom hook designed just to increment a counter.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
-import Table from "./components/Table";
+import CounterPage from "./pages/CounterPage";
 
 function App() {
   return (
@@ -26,6 +26,9 @@ function App() {
         </Route>
         <Route path="/table">
           <TablePage />
+        </Route>
+        <Route path="/counter">
+          <CounterPage initialCount={10} />
         </Route>
       </div>
     </div>
