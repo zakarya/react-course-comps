f74c81418c22fb3376254fc5a5e1f04e890fb736
Zack Vera <qphys50@gmail.com>
2024-11-15 16:56:08 -0800

    A ton of code added to the CounterPage.js. added an increment and decrement button, then a text input for only numbers to add larger amounts and submit button.

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,48 @@
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import { useState } from "react";
+import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <div className="flex flex-row">
+        <h1 className="text-lg">Count is {count}</h1>
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          value={valueToAdd || ""}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
 
